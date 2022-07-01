4b6addbb88ec08a89e9eb877e6acf32b4f3526e4
shlmvgleb <glebshalimov2003@mail.ru>
2022-07-01 16:18:11 +0300

    Validation feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
 import { CreateUserDTO } from "../users/dto/userDTO";
 import { AuthService } from "./auth.service";
 import { BanGuard } from "./ban.guard";
+import { ValidationPipe } from "../pipes/validation.pipe";
 
 
 @UseGuards(BanGuard)
@@ -15,6 +16,7 @@ export class AuthController {
     return this.authService.login(userDto);
   }
 
+  @UsePipes(ValidationPipe)
   @Post('/registration')
   registration(@Body() userDto: CreateUserDTO){
     return this.authService.registration(userDto);
--- src/roles/dto/roleDTO.ts
+++ src/roles/dto/roleDTO.ts
@@ -1,4 +1,9 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateRoleDTO {
+
+  @IsString({message: 'Должно быть строкой'})
   readonly role_name: string;
+  @IsString({message: 'Должно быть строкой'})
   readonly description: string;
 }
